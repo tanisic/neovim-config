8ab51efbba6f85b278957ce47ffc5c768a5ac24e
Tin Anišić <tin.anisic@tellus.hr>
2023-11-10 12:09:09 +0100

    Fix template string to double quoutes

--- lua/plugins/template-string.lua
+++ lua/plugins/template-string.lua
@@ -7,7 +7,7 @@ return {
       remove_template_string = true, -- remove backticks when there are no template strings
       restore_quotes = {
         -- quotes used when "remove_template_string" option is enabled
-        normal = [[']],
+        normal = [["]],
         jsx = [["]],
       },
     })
