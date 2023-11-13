084a665910599462dfba7b600f0cf0a05872f753
Tin Anišić <tin.anisic@tellus.hr>
2023-11-13 19:25:24 +0100

    Add emmet-ls, good for html chaining in .tsx files

---
+++ lua/plugins/emmet-ls.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        emmet_ls = {
+          filetypes = {
+            "html",
+            "typescriptreact",
+            "javascriptreact",
+            "css",
+            "sass",
+            "scss",
+            "less",
+            "javascript",
+            "typescript",
+            "markdown",
+          },
+          init_options = {
+            html = {
+              options = {
+                -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L26
+                ["bem.enabled"] = true,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
