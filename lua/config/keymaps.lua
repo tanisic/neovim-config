d2228d19bee075431b8b3b363d3b8c172b34c44a
Tin Anišić <tin.anisic@tellus.hr>
2023-11-14 19:57:17 +0100

    Switched from tsserver to typescript-tools, added keybindings for TS

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,5 @@ map("n", "<A-ŋ>", ":m .+1<CR>==") -- move line up(n)
 map("n", "<A- ̑>", ":m .-2<CR>==") -- move line down(n)
 map("v", "<A-ŋ>", ":m '>+1<CR>gv=gv") -- move line up(v)
 map("v", "<A- ̑>", ":m '<-2<CR>gv=gv") -- move line down(v)
+map("n", "<leader>cx", "<cmd>TSToolsRenameFile<cr>") -- TS rename file with updated imports
+map("n", "<leader>ci", "<cmd>TSToolsAddMissingImports<cr>") -- TS add missing imports
