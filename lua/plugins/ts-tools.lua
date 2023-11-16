68cf5ad009447cf6dc9669324206545c9cb71fef
Tin Anišić <tin.anisic@tellus.hr>
2023-11-16 11:45:18 +0100

    Add auto close tags, display all code actions TS

--- lua/plugins/ts-tools.lua
+++ lua/plugins/ts-tools.lua
@@ -1,5 +1,13 @@
 return {
   "pmizio/typescript-tools.nvim",
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  opts = {},
+  opts = {
+    settings = {
+      expose_as_code_action = "all",
+      jsx_close_tag = {
+        enable = true,
+        filetypes = { "javascriptreact", "typescriptreact" },
+      },
+    },
+  },
 }
