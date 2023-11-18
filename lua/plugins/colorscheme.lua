85fab7000f84b2cacdd369c932731226436ad21c
Tin Anišić <tin.anisic@tellus.hr>
2023-11-18 16:31:58 +0100

    Changed dark-theme.lua to colorscheme.lua

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+  -- add gruvbox
+  { "folke/tokyonight.nvim", opts = {}, lazy = false, priority = 1000 },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-storm",
+    },
+  },
+}
