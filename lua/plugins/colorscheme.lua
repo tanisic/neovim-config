6a6d259254f27a84d3c73bc4e6c31363fac483e8
Tin Anišić <tin.anisic@tellus.hr>
2024-01-20 12:20:30 +0100

    Changed colorscheme to kanagawa-wave

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,13 @@
 return {
   -- add gruvbox
-  { "folke/tokyonight.nvim", opts = {}, lazy = false, priority = 1000 },
+  -- { "folke/tokyonight.nvim", opts = {}, lazy = false, priority = 1000, config = false },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-storm",
+      colorscheme = "kanagawa-wave",
     },
   },
+
+  { "rebelot/kanagawa.nvim", lazy = false, config = true, priority = 1000 },
+  -- { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
 }
