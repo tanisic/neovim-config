982f1388ee420113deb08565442bb76416c00683
Tin Anišić <tin.anisic@tellus.hr>
2024-03-10 13:33:24 +0100

    ToggleTerm F4 as toggle floating terminal

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,3 +1,19 @@
 return {
-  { "akinsho/toggleterm.nvim", version = "*", config = true },
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = true,
+    cmd = "ToggleTerm",
+    build = ":ToggleTerm",
+    keys = { { "<F4>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal window" } },
+    opts = {
+      open_mapping = [[<F4>]],
+      direction = "float",
+      hide_numbers = true,
+      insert_mappings = true,
+      terminal_mappings = true,
+      start_in_insert = true,
+      close_on_exit = true,
+    },
+  },
 }
