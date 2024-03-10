6ebdb35b3493732ac50d2b7fc63469c013e41326
Tin Anišić <tin.anisic@tellus.hr>
2024-03-10 13:33:01 +0100

    Don't remember neovide window size

--- init.lua
+++ init.lua
@@ -4,7 +4,6 @@ require("config.lazy")
 if vim.g.neovide then
   vim.o.guifont = "JetbrainsMono_Nerd_Font:h14"
   vim.g.neovide_scroll_animation_far_lines = 0
-  vim.g.neovide_remember_window_size = true
   vim.g.neovide_cursor_animation_length = 0.1
   vim.g.neovide_cursor_trail_size = 0.2
 end
