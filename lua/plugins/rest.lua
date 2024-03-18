3a72c741ff4ccc765c1e80811801fa300de42347
Tin Anišić <tin.anisic@tellus.hr>
2024-03-18 09:41:48 +0100

    add luarocks nvim dependency to rest.nvim

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,9 +1,16 @@
 return {
-  "rest-nvim/rest.nvim",
-  dependencies = { { "nvim-lua/plenary.nvim" } },
-  config = function()
-    require("rest-nvim").setup({
-      --- Get the same options from Packer setup
-    })
-  end,
+  {
+    "vhyrro/luarocks.nvim",
+    config = function()
+      require("luarocks").setup({})
+    end,
+  },
+  {
+    "rest-nvim/rest.nvim",
+    ft = "http",
+    dependencies = { "luarocks.nvim" },
+    config = function()
+      require("rest-nvim").setup()
+    end,
+  },
 }
