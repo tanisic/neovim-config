978bcaaf2c5a469a0bdf5819d34175b004d18301
Tin <tin@Tins-Macbook-Pro.local>
2024-05-10 14:58:27 +0200

    Add auto remove unused imports on ts/js when file saved.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Auto remove unused imports on file save typescript and javascript only.
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = vim.api.nvim_create_augroup("TS_RemoveUnusedImports", { clear = true }),
+  desc = "TS_RemoveUnusedImports",
+  pattern = { "*.ts", "*.tsx", "*.js", "*.jsx" },
+  callback = function()
+    vim.cmd([[TSToolsRemoveUnusedImports]])
+    vim.cmd("write")
+  end,
+})
