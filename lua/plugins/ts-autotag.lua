39c8f69f699ba33929d10304eec77cef1a28981a
Tin <tin@Tins-Macbook-Pro.local>
2024-05-15 11:48:19 +0200

    Add nvim-ts-autotag as treesitter dependency

--- lua/plugins/ts-autotag.lua
+++ lua/plugins/ts-autotag.lua
@@ -1,7 +1,7 @@
 return {
-  { "nvim-treesitter/nvim-treesitter" },
   {
-    "windwp/nvim-ts-autotag",
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = { "windwp/nvim-ts-autotag" },
     opts = {
       auto_install = true,
       ensure_installed = {
