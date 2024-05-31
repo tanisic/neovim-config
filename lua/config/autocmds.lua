76218d93092a6af7b69cae72468e09167e303b62
Tin <tin@Tins-Macbook-Pro.local>
2024-05-31 16:05:46 +0200

    Disabled typescript-tools and using vtsls for typescript

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,12 +3,12 @@
 -- Add any additional autocmds here
 
 -- Auto remove unused imports on file save typescript and javascript only.
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = vim.api.nvim_create_augroup("TS_RemoveUnusedImports", { clear = true }),
-  desc = "TS_RemoveUnusedImports",
-  pattern = { "*.ts", "*.tsx", "*.js", "*.jsx" },
-  callback = function()
-    vim.cmd([[TSToolsRemoveUnusedImports]])
-    vim.cmd("write")
-  end,
-})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   group = vim.api.nvim_create_augroup("TS_RemoveUnusedImports", { clear = true }),
+--   desc = "TS_RemoveUnusedImports",
+--   pattern = { "*.ts", "*.tsx", "*.js", "*.jsx" },
+--   callback = function()
+--     vim.cmd([[TSToolsRemoveUnusedImports]])
+--     vim.cmd("write")
+--   end,
+-- })
