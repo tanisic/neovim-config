290e5975cdb149729f888bbf359e9bb95da21c1f
Tin <tin@Tins-Macbook-Pro.local>
2024-06-17 11:17:15 +0200

    Added package-info.nvim with keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,3 +16,37 @@ map("n", "<leader>š", "<cmd>LspRestart<cr>", { desc = "Restart LSP server" })
 
 map("n", "<leader>xc", "<Plug>RestNvim", { desc = "Execute REST query" })
 map("n", "<leader>cm", "<cmd>MarkdownPreviewToggle<cr>", { desc = "Render markdown" })
+
+local set_keymap = vim.api.nvim_set_keymap
+--
+-- package-info keymaps
+set_keymap(
+  "n",
+  "<leader>cpt",
+  "<cmd>lua require('package-info').toggle()<cr>",
+  { silent = true, noremap = true, desc = "Toggle" }
+)
+set_keymap(
+  "n",
+  "<leader>cpd",
+  "<cmd>lua require('package-info').delete()<cr>",
+  { silent = true, noremap = true, desc = "Delete package" }
+)
+set_keymap(
+  "n",
+  "<leader>cpu",
+  "<cmd>lua require('package-info').update()<cr>",
+  { silent = true, noremap = true, desc = "Update package" }
+)
+set_keymap(
+  "n",
+  "<leader>cpi",
+  "<cmd>lua require('package-info').install()<cr>",
+  { silent = true, noremap = true, desc = "Install package" }
+)
+set_keymap(
+  "n",
+  "<leader>cpc",
+  "<cmd>lua require('package-info').change_version()<cr>",
+  { silent = true, noremap = true, desc = "Change package version" }
+)
